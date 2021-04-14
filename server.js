00e5d90f250dd2d6b9ab9a76c00558835950639c
Christian Inarra <christiandev@MacBook-Pro-de-Christian.local>
2021-04-13 21:52:02 -0400

    change folder of the app

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-hospital'));
+app.use(express.static('./dist/app-hospital-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-hospital/'}),
+    res.sendFile('index.html', {root: 'dist/app-hospital-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
